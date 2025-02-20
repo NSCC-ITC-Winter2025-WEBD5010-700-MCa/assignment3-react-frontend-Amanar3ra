45ed83dec15595f0282e359d663b3b06c8281c3c
amanar3ra <amanarora3dec@gmail.com>
2025-02-20 15:15:45 -0400

    ICA Feb. 20,2025

--- src/components/books/BookCreate.jsx
+++ src/components/books/BookCreate.jsx
@@ -7,6 +7,10 @@ const BookCreate = () => {
     const queryClient = useQueryClient()
     const navigate = useNavigate()
 
+    const collectData = (data) => {
+        createBookMutation.mutate(data)
+    }
+
     const createBookMutation = useMutation({
         mutationFn: async (data) => {
             const response = await fetch(`http://localhost:3000/books/`, {
@@ -25,7 +29,7 @@ const BookCreate = () => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit(createBookMutation.mutate)}
+            <form onSubmit={handleSubmit(collectData)}
                 className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md space-y-4" >
                 <h2 className="text-xl font-semibold text-center">Create a New Book</h2>
 
@@ -35,30 +39,34 @@ const BookCreate = () => {
                     type="text" placeholder="Title"
                     className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                     />
+                    {errors.title && <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>}
                 </div>
 
                 <div className="space-y-2">
                     <label className="block text-gray-700">Author</label>
-                    <input {...register('author')} 
+                    <input {...register('author', { required: 'Author is required' })} 
                     type="text" placeholder="Author"
                     className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                     />
+                    {errors.author && <p className="text-red-500 text-sm mt-1">{errors.author.message}</p>}
                 </div>
 
                 <div className="space-y-2">
                     <label className="block text-gray-700">Published Year</label>
-                    <input {...register('published_year')} 
+                    <input {...register('published_year', { required: 'Year is required' , min: { value: 1700, message: 'Year must be at least 1700' }})} 
                     type="text" placeholder="Publication year"
                     className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                     />
+                    {errors.published_year && <p className="text-red-500 text-sm mt-1">{errors.published_year.message}</p>}
                 </div>
 
                 <div className="space-y-2">
                     <label className="block text-gray-700">Genre</label>
-                    <input {...register('genre')} 
+                    <input {...register('genre', { required: 'Genre is required' })} 
                     type="text" placeholder="Genre"
                     className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                     />
+                    {errors.genre && <p className="text-red-500 text-sm mt-1">{errors.genre.message}</p>}
                 </div>
 
                 <button type="submit"
--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -11,31 +11,6 @@ import {
   faBook
 } from '@fortawesome/free-solid-svg-icons';
 
-const SidebarItem = ({ href, label, isCollapsed, icon }) => {
-  const location = useLocation();
-  const isActive = location.pathname === href;
-
-  return (
-    <li className="group">
-      <Link
-        to={href}
-        className={`flex items-center py-2.5 px-4 hover:bg-white transition ${
-          isActive ? 'bg-white border-y-2 border-black-500' : ''
-        }`}
-      >
-        <FontAwesomeIcon icon={icon} />
-        <span
-          className={`text-black text-base font-medium leading-tight ml-4 text-nowrap ${
-            isCollapsed ? 'hidden' : 'block'
-          }`}
-        >
-          {label}
-        </span>
-      </Link>
-    </li>
-  );
-};
-
 const Sidebar = ({ isCollapsed, toggleSidebar }) => {
   const sidebarItems = [
     { href: '/admin/home', label: 'Dashboard', icon: faChartPie },
@@ -46,6 +21,33 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
     { href: '/admin/subscriptions', label: 'Subscriptions', icon: faBoxOpen },
     { href: '/admin/books', label: 'Books', icon: faBook }
   ];
+
+  const SidebarItem = ({ href, label, isCollapsed, icon }) => {
+    const location = useLocation();
+    const isActive = location.pathname === href;
+  
+    return (
+      <>
+      <li className="group">
+        <Link
+          to={href}
+          className={`flex items-center py-2.5 px-4 hover:bg-white transition ${
+            isActive ? 'bg-white border-y-2 border-black-500' : ''
+          }`}
+        >
+          <FontAwesomeIcon icon={icon} />
+          <span
+            className={`text-black text-base font-medium leading-tight ml-4 text-nowrap ${
+              isCollapsed ? 'hidden' : 'block'
+            }`}
+          >
+            {label}
+          </span>
+        </Link>
+      </li>
+      </>
+    );
+  };
 
   return (
     <aside
