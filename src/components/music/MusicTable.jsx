0ae0bd52507e4d1963ac5c713dacab179b476c6c
amanar3ra <amanarora3dec@gmail.com>
2025-03-10 17:01:08 -0300

    Working music component

---
+++ src/components/music/MusicTable.jsx
@@ -0,0 +1,72 @@
+import { useMutation , useQueryClient } from "@tanstack/react-query";
+import { Link, useNavigate } from "react-router-dom";
+
+const MusicTable = ({music}) => {
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const deleteBookMutation = useMutation({
+
+    mutationFn: async (musicId) => {
+      const response = await fetch(`${import.meta.env.VITE_MUSIC_API_URL}/${musicId}`, {
+        method: 'DELETE'
+      })
+      return response.json()
+    },
+    onSuccess : () => {
+      queryClient.invalidateQueries(['musicData'])
+    },
+
+    onError: (error) => {
+      alert('Unable to delete')
+    }
+  })
+
+  const handleDelete = (musicId) => {
+    //Send a delete request to our api to delete the selected record
+    if(window.confirm(`Are you sure you wish to delete record ${musicId}`)){
+      deleteBookMutation.mutate(musicId)
+    }
+  }
+    return (
+    <>
+      <p><Link to="/admin/books/create"
+      className="bg-blue-700 text-white text-sm px-2 py-0 rounded-md shadow-sm hover:bg-blue-600 transition duration-200">
+        Add New Song</Link></p>
+      <table className="w-full border-collapse border border-gray-200">
+      <thead className="bg-gray-200">
+      <tr>
+      <th className="border border-gray-300 px-4 py-2 text-left">ID</th>
+      <th className="border border-gray-300 px-4 py-2 text-left">Title</th>
+      <th className="border border-gray-300 px-4 py-2 text-left">Artist</th>
+      <th className="border border-gray-300 px-4 py-2 text-left">Released Year</th>
+      <th className="border border-gray-300 px-4 py-2 text-left">Genre</th>
+      <th className="border border-gray-300 px-4 py-2">Ratings</th>
+      </tr>
+      </thead>
+      <tbody>
+      {music.map(music=>{
+        return(
+         <tr key={music.id} className="hover:bg-gray-100">
+         <td className="border border-gray-300 px-4 py-2">{music.id}</td>
+         <td className="border border-gray-300 px-4 py-2">{music.title}</td>
+         <td className="border border-gray-300 px-4 py-2">{music.artist}</td>
+         <td className="border border-gray-300 px-4 py-2">{music.released_year}</td>
+         <td className="border border-gray-300 px-4 py-2">{music.ratings.rym}</td>
+         <td className="border border-gray-300 px-4 py-2 text-center space-x-1">
+          <button className="bg-green-500 text-white px-2 py-1 text-sm rounded hover:bg-green-600">Details</button>
+          <button onClick={() => navigate(`/admin/books/${music.id}/edit`)} className="bg-blue-500 text-white px-2 py-1 text-sm rounded hover:bg-blue-600">Edit</button>
+          <button onClick={ () => {handleDelete(book.id) }} className="bg-red-500 text-white px-2 py-1 text-sm rounded hover:bg-red-600">Delete</button>
+         </td>
+         </tr>)
+  
+      })}
+  
+     
+      </tbody>
+      </table>
+    </>
+    )
+}
+  
+export default MusicTable;
--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,11 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import { Outlet, useLocation } from "react-router-dom";
+// import BooksTable from "../components/books/BooksTable"
+
 const Music = () => {
+
+    //Get the current location information
+    const location = useLocation()
+
+    const { isPending, error, data: music } = useQuery({
+        queryKey: ['musicData'],
+        queryFn: async () => {
+            const response = await fetch(`${import.meta.env.VITE_MUSIC_API_URL}`);
+            return response.json() //returns a promise of our data
+        },
+        staleTime: Infinity //cache the data & store it locally
+    }) 
+
+    if(error) return <div>{`An error has occured: ${error.message}`}</div>;
+
     return (
       <div>
-        <h1 className="text-2xl font-bold">Music</h1>
-        <p>Welcome to the admin Musics.</p>
-      </div>
+        <h1 className="text-2xl font-bold">Books</h1>
+
+        {location.pathname === '/admin/books' ? (
+            isPending ?
+            <div>Loading...</div> 
+            :
+            <MusicTable music = {music} />
+        ) 
+        
+        : 
+        
+        <Outlet/>
+        }
+    </div>
     );
 };
-  
+
 export default Music;
   
