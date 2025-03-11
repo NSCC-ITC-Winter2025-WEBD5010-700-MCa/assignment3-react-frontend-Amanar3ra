dbb138608fc3ab88d50891f301645ea9d23f3d9d
amanar3ra <amanarora3dec@gmail.com>
2025-03-11 12:49:05 -0300

    Enabled music create form

---
+++ src/components/music/MusicCreate.jsx
@@ -0,0 +1,37 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import MusicForm from './MusicForm';
+
+const MusicCreate = () => {
+    const queryClient = useQueryClient()
+    const navigate = useNavigate()
+
+    const processData = (data) => {
+        createMusicMutation.mutate(data)
+    }
+
+    const createMusicMutation = useMutation({
+        mutationFn: async (data) => {
+            const response = await fetch(`${import.meta.env.VITE_MUSIC_API_URL}`, {
+              method: 'POST',
+              header: {'Content-Type' : 'application/json'},
+              body: JSON.stringify(data)
+            })
+
+            return response.json()
+        },
+        onSuccess : () => {
+            queryClient.invalidateQueries(['musicData']),
+            navigate('/admin/music')
+        },
+    })
+
+    return (
+        <div className="max-w-lg mx-auto bg-white shadow-md rounded-lg p-6">
+            <h2 className="text-xl font-semibold text-center">Create a New Song</h2>
+            <MusicForm onDataCollection = {processData}/>
+        </div>
+    )
+}
+
+export default MusicCreate;
